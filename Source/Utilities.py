70c038ed646a111045acdf2d3c0f83848c95c0ba
SpreadSheets600 <Sohammaity006@gmail.com>
2025-01-06 17:11:08 +0530

    Improved Utilities And Refactor

--- Source/Utilities.py
+++ Source/Utilities.py
@@ -1,79 +1,34 @@
 import re
 import json
 import unicodedata
-import humanfriendly
 from discord_webhook import DiscordWebhook
+
+with open("source/Pokemon", "r", encoding="utf8") as file:
+    pokemon_list = file.read()
+
+
+def solve(message):
+    hint = []
+    for i in range(15, len(message) - 1):
+        if message[i] != "\\":
+            hint.append(message[i])
+    hint_string = "".join(hint)
+    hint_replaced = hint_string.replace("_", ".")
+    return re.findall("^" + hint_replaced + "$", pokemon_list, re.MULTILINE)
 
 
 def read_config(filename="Source/Config.json"):
-
     with open(filename, "r") as f:
         return json.load(f)
 
 
 def send_log(embed, WEBHOOK_URL):
-
     webhook = DiscordWebhook(url=WEBHOOK_URL, username="Pokefier Log")
     webhook.add_embed(embed)
     webhook.execute()
 
-def is_shard_buy_message(message, whitelisted_channels, POKETWO_ID):
-    
-    return (
-        message.author.id == POKETWO_ID
-        and message.channel.id in whitelisted_channels
-        and "Are you sure you want to exchange".lower() in message.content.lower()
-    )
-
-
-def is_spawn_message(message, whitelisted_channels, POKETWO_ID):
-
-    return (
-        message.author.id == POKETWO_ID
-        and message.channel.id in whitelisted_channels
-        and len(message.embeds) > 0
-        and "wild pokémon has appeared".lower() in message.embeds[0].title.lower()
-    )
-
-
-def is_captcha_message(message, whitelisted_channels, id, POKETWO_ID):
-
-    return (
-        message.author.id == POKETWO_ID
-        and message.channel.id in whitelisted_channels
-        and f"https://verify.poketwo.net/captcha/{id}" in message.content
-    )
-
-
-def is_pokemon_caught_message(message, whitelisted_channels, id, POKETWO_ID):
-
-    return (
-        message.author.id == POKETWO_ID
-        and message.channel.id in whitelisted_channels
-        and f"<@{id}>" in message.content
-        and "you caught a level" in message.content.lower()
-    )
-
-def is_pokemon_wrong(message, whitelisted_channels, id, POKETWO_ID):
-
-    return (
-        message.author.id == POKETWO_ID
-        and message.channel.id in whitelisted_channels
-        and f"<@{id}>" in message.content
-        and "is the wrong pokémon" in message.content.lower()
-    )
-
-def not_enough_sahards(message, whitelisted_channels, id, POKETWO_ID):
-
-    return (
-        message.author.id == POKETWO_ID
-        and message.channel.id in whitelisted_channels
-        and f"<@{id}>" in message.content
-        and "You don't have enough shards" in message.content.lower()
-    )
 
 def extract_pokemon_data(text):
-
     pattern = r"Level (\d+) ([^(]+) \(([\d.]+)%\)[.!]*"  # Pattern To Extract Level, Name, And IV
     match = re.search(pattern, text)
 
@@ -91,18 +46,11 @@ def extract_pokemon_data(text):
 
 
 def load_pokemon_data():
-
     with open("Source/Data.json", "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def convert_seconds(seconds):
-
-    return humanfriendly.format_timespan(seconds)
-
-
 def remove_diacritics(input_str):
-
     normalized_str = unicodedata.normalize("NFD", input_str)
     ascii_str = "".join(c for c in normalized_str if unicodedata.category(c) != "Mn")
     return ascii_str
